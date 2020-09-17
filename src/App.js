aaf0ba350ba154657c8d4608cbe40659b4815029
Wayne Cassity <webdev.cassity@gmail.com>
2020-09-16 22:45:28 -0700

    completed header-title-dropdown, Hooks and Api connected, basic styling, --next work on infoBoxs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+	const [countries, setCountries] = useState([]);
+	const [country, setCountry] = useState(["worldwide"]);
+
+	useEffect(() => {
+		const getCountriesData = async () => {
+			await fetch("https://disease.sh/v3/covid-19/countries")
+				.then((response) => response.json())
+				.then((data) => {
+					const countries = data.map((country) => ({
+						name: country.country, // United States, United Kingdom
+						value: country.countryInfo.iso2, // US, UK, FR
+					}));
+					setCountries(countries);
+				});
+		};
+		getCountriesData();
+	}, []);
+
+	const onCountryChange = (event) => {
+		const countryCode = event.target.value;
+		setCountry(countryCode);
+	};
+
 	return (
 		// BEM naming convention //
-		<div className='app'>
-			<h1>Building the Covid Tracker</h1>
+		<div className="app">
+			<div className="app__header">
+				<h1>COVID 19 TRACKER</h1>
+				<FormControl className="app__dropdown">
+					<Select variant="outlined" onChange={onCountryChange} value={country}>
+						<MenuItem value="worldwide">Worldwide</MenuItem>
+						{countries.map((country) => (
+							<MenuItem value={country.value}>{country.name}</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+			</div>
+			{/* Header */}
+
+			{/* Title + input dropdown field*/}
+
+			{/* InfoBoxs */}
+			{/* InfoBoxs */}
+			{/* InfoBoxs */}
+
+			{/* Table */}
+			{/* Graph */}
+
+			{/* Map */}
 		</div>
 	);
 }
